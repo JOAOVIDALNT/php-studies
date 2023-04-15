6aa573e5571dc35f361abd357a91d129f780ea3f
João Vidal <dev.joaovidal@gmail.com>
2023-04-15 12:10:53 -0300

    frustating connects

--- courses/php-essencial/05-projeto_pt1/cadastrar_clientes.php
+++ courses/php-essencial/05-projeto_pt1/cadastrar_clientes.php
@@ -1,11 +1,20 @@
 <?php
 
-$erro = false;
-if(count($_POST)) {
-    $nome = $_POST['nome'];
-    $email = $_POST['email'];
-    $telefone = $_POST['telefone'];
-    $nascimento = $_nascimento['nascimento'];
+function limpar_texto($str) {
+    return preg_replace("/[^0-9]/", "", $str); 
+}
+
+
+if(count($_POST) > 0) {
+
+
+    include "conexao.php";
+    $erro = false;
+
+     $nome = $_POST['nome'];
+     $email = $_POST['email'];
+     $telefone = $_POST['telefone'];
+     $nascimento = $_POST['nascimento'];
 
     if(empty($nome)) {
         $erro = "Preencha o nome";
@@ -14,12 +23,37 @@ if(count($_POST)) {
     if(empty($email)) {
         $erro = "Preencha o email";
     }
+    if(empty($email) || !filter_var($email, FILTER_VALIDATE_EMAIL)) {
+        $erro = "Preencha o email";
+    }
 
+    if(!empty($nascimento)) {
+        $pedacos = explode('/', $nascimento);
+        if(count($pedacos) == 3) {
+            $nascimento = implode('-', array_reverse($pedacos));
+        } else {
+            $erro = "A data de nascimento deve seguir o padrão dia/mẽs/ano.";
+        }
+    }
 
+    if(!empty($telefone)) {
+        $telefone = limpar_texto($telefone);
+        if(strlen($telefone) != 11) {
+            $erro = "O telefone deve ser preenchido no padrão (11)99999-9999";
+        }
+    }
 
     if($erro) {
         echo "<p><strong>ERRO: $erro</strong></p>";
-    }
+    } else {
+        $sql = "INSERT INTO clientes (nome, email, telefone, dara_nascimento, data_cadastro) 
+        VALUES ('$nome', '$email', '$telefone', '$nascimento', NOW())";
+
+        $result = mysqli_query($sql);
+            echo "<p><strong>Cliente cadastrado com sucesso!!</strong></p>";
+            unset($_POST);
+        }
+
 }
 
 ?>
@@ -40,23 +74,28 @@ if(count($_POST)) {
     <!-- cadastro feito dentro desta página (action em branco) -->
         <div>
             <label hidden>Nome</label>
-            <input type="text" name="nome" placeholder="Nome">
+            <input type="text" name="nome" placeholder="Nome" 
+            value="<?php if(isset($_POST['nome'])) echo $_POST['nome'] ?>">
+            <!-- define que se o nome estiver setado o campo mantem o valor inserido caso ocorra algum erro -->
         </div>
         <div>
             <label hidden>Email</label>
-            <input type="text" name="email" placeholder="E-mail">
+            <input type="text" name="email" placeholder="E-mail"
+            value="<?php if(isset($_POST['email'])) echo $_POST['email'] ?>">
         </div>
         <div>
             <label hidden>Telefone</label>
-            <input type="text" name="telefone" placeholder="Telefone">
+            <input type="text" name="telefone" placeholder="Telefone" placeholder="(11)99999-9999"
+            value="<?php if(isset($_POST['telefone'])) echo $_POST['telefone'] ?>">
         </div>
         <div>
             <label hidden>Data de Nascimento</label>
-            <input type="text" name="nascimento" placeholder="Data de Nascimento">
+            <input type="text" name="nascimento" placeholder="Data de Nascimento"
+            value="<?php if(isset($_POST['nascimento'])) echo $_POST['nascimento'] ?>">
         </div>
 
         <div>
-            <button type="submit">Salvar Cliente</button>
+            <button type="submit" name="submit">Salvar Cliente</button>
         </div>
     </form>
 </body>
--- courses/php-essencial/05-projeto_pt1/conexao.php
+++ courses/php-essencial/05-projeto_pt1/conexao.php
@@ -1,12 +1,16 @@
 <?php
-
 $host = "localhost:3306";
 $db = "php_projeto_pt1";
 $user = "root";
 $pass = "root";
 
-$mysqli = new mysqli($host, $user, $pass, $db);
-
-if($mysqli->connect_errno) {
-    die("Falha na conexão com o banco de dados");
-}
+//$conn = mysqli_connect($host, $user, $pass, $db);
+//
+//if($conn->connect_errno) {
+//    die("Falha na conexão com o banco de dados");
+//}
+ try {
+     $conn = new PDO("mysql:host=$host;dbname=$db", $user, $pass);
+ } catch (PDOException $e) {
+     echo "Connection failed: " . $e->getMessage();
+ }
