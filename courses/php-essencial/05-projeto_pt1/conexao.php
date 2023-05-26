bfe03990a58598daf9313124aad59edec33c9d38
João Vidal <dev.joaovidal@gmail.com>
2023-05-26 09:29:16 -0300

    merge try

--- courses/php-essencial/05-projeto_pt1/conexao.php
+++ courses/php-essencial/05-projeto_pt1/conexao.php
@@ -2,9 +2,10 @@
 $host = "localhost";
 $db = "php-essencial";
 $user = "root";
-$pass = "";
+$pass = "root";
+$port = 3306;
 
-$conn = new mysqli($host, $user, $pass, $db);
+$conn = new mysqli($host, $user, $pass, $db, $port);
 
 if($conn->connect_errno) {
    die("Falha na conexão com o banco de dados");
