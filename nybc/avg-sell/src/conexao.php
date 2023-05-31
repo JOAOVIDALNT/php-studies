5954cd7d1c8acb10222658eb7f0e91b04e7862ab
João Vidal <dev.joaovidal@gmail.com>
2023-05-31 16:23:45 -0300

    avg-sales project

--- nybc/avg-sell/src/conexao.php
+++ nybc/avg-sell/src/conexao.php
@@ -11,14 +11,26 @@ try {
         $user, $password);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-    $sql = "select SUBSTRING (pm.data_integracao::text, 0, 8), sum(pd.quant::numeric), pd.produto
+    $sql = "SELECT SUBSTRING(pm.data_integracao::text, 0, 8) AS data, SUM(pd.quant::numeric) AS quantidade, pd.produto
             FROM pedido_detalhe_produto_integracao_bling pd
             INNER JOIN pedido_master_produto_integracao_bling pm ON pd.pedido_integracao = pm.pedido_integracao
-            group by SUBSTRING (pm.data_integracao::text, 0, 8),  pd.produto limit 10";
+            GROUP BY SUBSTRING(pm.data_integracao::text, 0, 8), pd.produto
+            LIMIT 10";
+
+    $result = array();
 
     foreach ($pdo->query($sql) as $row) {
 
+        $obj = new stdClass();
+        $obj->data = $row['data'];
+        $obj->quantidade = $row['quantidade'];
+        $obj->produto = $row['produto'];
+
+
+        $result[] = $obj;
     }
+
+    return $result;
 
 } catch (PDOException $e) {
     die($e->getMessage());
